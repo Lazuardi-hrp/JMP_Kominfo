9dd3fd2d841c845cc84fbaa0466794e019e4e2d9
Muhammad Lazuardi Harahap <anakiteknologi@gmail.com>
2024-08-06 14:38:10 +0700

    login

---
+++ app/src/main/java/com/example/kominfo_md/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kominfo_md;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLogin.setBackgroundColor(Color.RED);
+
+            }
+        });
+    }
+}
