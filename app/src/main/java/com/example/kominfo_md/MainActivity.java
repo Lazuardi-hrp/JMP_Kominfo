4d8bfe2308ee75a49a4f46f2acfa460be815bdaf
Muhammad Lazuardi Harahap <anakiteknologi@gmail.com>
2024-08-07 14:09:24 +0700

    add splashscreen

--- app/src/main/java/com/example/kominfo_md/MainActivity.java
+++ app/src/main/java/com/example/kominfo_md/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kominfo_md;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button btnLogin = findViewById(R.id.btnLogin);
+        final
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btnLogin.setBackgroundColor(Color.RED);
 
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pindah ke halaman registrasi
+                Intent intent = new Intent(MainActivity.this, RegActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/kominfo_md/RegActivity.java
+++ app/src/main/java/com/example/kominfo_md/RegActivity.java
@@ -2,7 +2,10 @@ package com.example.kominfo_md;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class RegActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        final Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pindah ke halaman registrasi
+                Intent intent = new Intent(RegActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
