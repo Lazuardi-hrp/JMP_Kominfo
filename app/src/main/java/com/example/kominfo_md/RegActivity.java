25447a4a8053dbebdb01beb05b7c7d4264a07e06
Muhammad Lazuardi Harahap <anakiteknologi@gmail.com>
2024-08-07 10:22:59 +0700

    loginn

---
+++ app/src/main/java/com/example/kominfo_md/RegActivity.java
@@ -0,0 +1,14 @@
+package com.example.kominfo_md;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
